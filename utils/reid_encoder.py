7dd4606cdb3477ec222a8851f971be032946df38
lj <852816672@qq.com>
2024-07-18 11:53:13 +0800

    reid encoder

---
+++ utils/reid_encoder.py
@@ -0,0 +1,139 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision import models
+
+
+'''
+base on: https://github.com/NirAharon/BoT-SORT/blob/main/fast_reid/fast_reid_interfece.py#L52
+'''
+
+
+def postprocess(features):
+    # Normalize feature to compute cosine distance
+    features = F.normalize(features)
+    features = features.cpu().data.numpy()
+    return features
+
+
+def preprocess(image, input_size):
+    if len(image.shape) == 3:
+        padded_img = np.ones((input_size[1], input_size[0], 3), dtype=np.uint8) * 114
+    else:
+        padded_img = np.ones(input_size) * 114
+    img = np.array(image)
+    r = min(input_size[1] / img.shape[0], input_size[0] / img.shape[1])
+    resized_img = cv2.resize(
+        img,
+        (int(img.shape[1] * r), int(img.shape[0] * r)),
+        interpolation=cv2.INTER_LINEAR,
+    )
+    padded_img[: int(img.shape[0] * r), : int(img.shape[1] * r)] = resized_img
+
+    return padded_img, r
+
+
+class ReIDEncoder:
+    def __init__(self, input_size, weights_path, batch_size=1):
+        super(ReIDEncoder, self).__init__()
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.batch_size = batch_size
+
+        if weights_path is not None:
+            pretraine = False
+        else:
+            pretraine = True
+        self.model = models.resnet50(pretrained=pretraine)  # todo use tensorRT or pytorch
+
+        self.model.eval()
+
+        if weights_path is not None:
+            self.model.load_state_dict(torch.load(weights_path))
+
+        if self.device != 'cpu':
+            self.model = self.model.eval().to(self.device).half()
+        else:
+            self.model = self.model.eval()
+        # use the encoded features
+        self.model.fc = nn.Sequential()
+        self.pH, self.pW = input_size
+
+    def inference(self, image, detections):
+
+        if detections is None or np.size(detections) == 0:
+            return []
+
+        H, W, _ = np.shape(image)
+
+        batch_patches = []
+        patches = []
+        for d in range(np.size(detections, 0)):
+            tlbr = detections[d, :4].astype(np.int_)
+            tlbr[0] = max(0, tlbr[0])
+            tlbr[1] = max(0, tlbr[1])
+            tlbr[2] = min(W - 1, tlbr[2])
+            tlbr[3] = min(H - 1, tlbr[3])
+            patch = image[tlbr[1]:tlbr[3], tlbr[0]:tlbr[2], :]
+
+            # the model expects RGB inputs
+            patch = patch[:, :, ::-1]
+
+            # Apply pre-processing to image.
+            patch = cv2.resize(patch, tuple([self.pW, self.pH]), interpolation=cv2.INTER_LINEAR)
+            # patch, scale = preprocess(patch, self.cfg.INPUT.SIZE_TEST[::-1])
+
+            # plt.figure()
+            # plt.imshow(patch)
+            # plt.show()
+
+            # Make shape with a new batch dimension which is adapted for network input
+            patch = torch.as_tensor(patch.astype("float32").transpose(2, 0, 1))
+            patch = patch.to(device=self.device).half()
+
+            patches.append(patch)
+
+            if (d + 1) % self.batch_size == 0:
+                patches = torch.stack(patches, dim=0)
+                batch_patches.append(patches)
+                patches = []
+
+        if len(patches):
+            patches = torch.stack(patches, dim=0)
+            batch_patches.append(patches)
+
+        features = np.zeros((0, 2048))
+        # features = np.zeros((0, 768))
+
+        for patches in batch_patches:
+
+            # Run model
+            patches_ = torch.clone(patches)
+            pred = self.model(patches)
+            pred[torch.isinf(pred)] = 1.0
+
+            feat = postprocess(pred)
+
+            nans = np.isnan(np.sum(feat, axis=1))  # todo check functon, to delet
+            if np.isnan(feat).any():
+                for n in range(np.size(nans)):
+                    if nans[n]:
+                        # patch_np = patches[n, ...].squeeze().transpose(1, 2, 0).cpu().numpy()
+                        patch_np = patches_[n, ...]
+                        patch_np_ = torch.unsqueeze(patch_np, 0)
+                        pred_ = self.model(patch_np_)
+
+                        patch_np = torch.squeeze(patch_np).cpu()
+                        patch_np = torch.permute(patch_np, (1, 2, 0)).int()
+                        patch_np = patch_np.numpy()
+
+                        plt.figure()
+                        plt.imshow(patch_np)
+                        plt.show()
+
+            features = np.vstack((features, feat))
+
+        return features
